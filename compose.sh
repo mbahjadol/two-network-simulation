c800678190f2a720c14de54f24fb1f1b4a749479
mbahjadol <mbah.jadol@gmail.com>
2025-10-21 08:52:33 +0700

    compose.sh

---
+++ compose.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+function get_help() {
+  echo "Two-Network Simulation Environment"
+  echo  
+  echo "Usage: $0 [up|down|start|stop|restart]"
+  echo
+  echo "Commands:"
+  echo "  up        - 🚀 Create and Start the containers in detached mode"
+  echo "  down      - 🛑 Stop and remove the containers"
+  echo "  start     - ▶️ Start existing containers"
+  echo "  stop      - ⏸️ Stop running containers"
+  echo "  restart   - 🔄 Restart the containers"
+  echo "  help      - ❓ Show this help message"
+  echo
+}
+
+if [ "$1" = "up" ]; then
+  echo "🚀 Create and starting containers..."
+  docker compose up -d
+elif [ "$1" = "down" ]; then
+  echo "🛑 Stopping and removing containers..."
+  docker compose down
+elif [ "$1" = "start" ]; then
+  echo "▶️ Starting existing containers..."
+  docker compose start -d
+elif [ "$1" = "stop" ]; then
+  echo "⏸️ Stopping running containers..."
+  docker compose stop
+elif [ "$1" = "restart" ]; then
+  echo "🔄 Restarting containers..."
+  docker compose restart
+elif [ -z "$1" ] || [ "$1" = "help" ] || [ "$1" = "--help" ] || [ "$1" = "-h" ]; then
+  get_help
+else
+  echo "❌ Invalid parameter: $1"
+  echo "Usage: $0 [up|down|start|stop|restart]"
+  exit 1
+fi
+
